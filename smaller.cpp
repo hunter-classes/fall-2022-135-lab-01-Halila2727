56785709c19531bed11c382c8a004dddc78bd64c
Halil Akca <halilakca321@gmail.com>
2022-09-01 11:14:03 -0400

    Created the smaller function

---
+++ smaller.cpp
@@ -0,0 +1,19 @@
+/*
+	Author: Halil Akca
+	Course: CSCI-135
+	Instructor: Mike Zamansky
+	Assignment: Lab 01, Task A
+
+	This program will ask the user to input 2 integer numbers and print out the smaller of the two
+*/
+int smaller(int a, int b)
+{
+	if(a < b)
+	{
+		return a;
+	}
+	else
+	{
+		return b;
+	}
+}
