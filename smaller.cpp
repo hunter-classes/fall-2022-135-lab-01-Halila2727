a859455a916ecff2c8caf93942fe1d5367317738
Halil Akca <halilakca321@gmail.com>
2022-09-01 23:37:26 -0400

    Created the main function which outputs the smaller of two numbers

--- smaller.cpp
+++ smaller.cpp
@@ -6,6 +6,8 @@
 
 	This program will ask the user to input 2 integer numbers and print out the smaller of the two
 */
+#include <iostream>
+
 int smaller(int a, int b)
 {
 	if(a < b)
@@ -16,4 +18,20 @@ int smaller(int a, int b)
 	{
 		return b;
 	}
+}
+
+int main()
+{
+	int firstNum=0;
+
+	int secondNum=0;
+
+	std::cout << "Enter the first number: ";
+	std::cin >> firstNum;
+
+	std::cout << "Enter the second number: ";
+	std::cin >> secondNum;
+
+	std::cout << "The smaller of the two is " << smaller(firstNum, secondNum) << ".";
+	return 0;
 }
